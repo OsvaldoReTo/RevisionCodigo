6b4019239ae12b68aac9b24dbcd886e2c57a6a0c
OsvaldoReTo <resendiz.tolentino.osvaldo@gmail.com>
2022-09-30 14:41:31 -0500

    chore| Se solucionaron errores en la llamada de la información de codigo-otros-1

--- codigo-otros-1-main/assets/js/app.js
+++ codigo-otros-1-main/assets/js/app.js
@@ -1,22 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); //tenía selector equivocado
+const $b = document.querySelector('.blog'); //tenía selector equivocado
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //se usó función asíncrona
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  try {const response = await fetch(`${usersEndpoint}/${username}`);
+  let data = await response.json(); // se genera objeto json de la información de la API
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
-
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`; //se añadió back thicks para todos estos campos
+  $l.textContent = `${data.location}`;
+}catch(error){
+      handleError(error)
+}//se añade try/catch para la función asíncrona
+}//async function
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski')//.catch(handleError); método de más si se añada ya un catch al displayUser

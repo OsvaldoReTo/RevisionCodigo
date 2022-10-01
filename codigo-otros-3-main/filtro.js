2f17c89a75b6081ee9c050e5b85d294f63db049b
OsvaldoReTo <resendiz.tolentino.osvaldo@gmail.com>
2022-09-30 19:10:57 -0500

    chore| Se corrige el filtro del ejercicio 3

--- codigo-otros-3-main/filtro.js
+++ codigo-otros-3-main/filtro.js
@@ -1,5 +1,6 @@
 // Tenemos un li de productos
 
+//lista de productos, arreglo de objetos tipo json
 const productos = [
   {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
   {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
@@ -8,9 +9,12 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+//llamada de el elemento div en el HTML y el input($i)
+const li = document.getElementById("lista-de-productos")//error aquí se uso un método no útil, es por Id no por name
+const $i = document.querySelector('input');//se solicita una class pero en su lugar debería ser un tag name
 
+//función displayProductos
+function displayProductos(productos) {
 for (let i = 0; i < productos.length; i++) {
   var d = document.createElement("div")
   d.classList.add("producto")
@@ -27,19 +31,34 @@ for (let i = 0; i < productos.length; i++) {
 
   li.appendChild(d)
 }
+}//displayProductos
 
+
+//función de filtrado
+const filtrado = (productos = [], texto) => {
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+}  
+
+//llamada de la función para desplegar los productos
 displayProductos(productos)
+
+//llamada del botón de filtrado
 const botonDeFiltro = document.querySelector("button");
 
+//evento click en el botón
 botonDeFiltro.onclick = function() {
+
+  //limpieza de pantalla
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
 
+  //Filtrar elementos por su nombre
   const texto = $i.value;
   console.log(texto);
   const productosFiltrados = filtrado(productos, texto );
 
+  //Generar los elementos resultados del filtro
   for (let i = 0; i < productosFiltrados.length; i++) {
     var d = document.createElement("div")
     d.classList.add("producto")
@@ -56,8 +75,4 @@ botonDeFiltro.onclick = function() {
   
     li.appendChild(d)
   }
-}
-
-const filtrado = (productos = [], texto) => {
-  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+}//botón de filtrado 
